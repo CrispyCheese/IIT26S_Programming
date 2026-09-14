180cf717f52f1bb7687cd01ae05513effd3b44a7
Juuso Reunamo <juuso.reunamo@gmail.com>
2026-09-14 11:20:55 +0300

    Finished tasks in week 1 folder

---
+++ Week 1/A1_T7.py
@@ -0,0 +1,7 @@
+print ("Calculate fuel consumption.")
+Feed = input("Enter travel distance(kilometers): ")
+Distance = int(Feed)
+Feed = input("Enter fuel usage(liters): ")
+FuelUsage = int(Feed)
+Consumption = round(FuelUsage / Distance * 100)
+print (f"Fuel consumption is {Consumption} l per 100 km")
